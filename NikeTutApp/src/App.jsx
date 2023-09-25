55805085137dc767ba3d17314500a16d7bf5dca1
Amir Mounir <amounir@turndigital.net>
2023-09-25 10:36:03 +0300

    init: init the repo

---
+++ NikeTutApp/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline relative  hover:left-52 transition-all">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
